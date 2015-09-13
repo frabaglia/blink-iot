f26fc54aee9e9ed10265a7a665b853b71fbfe465
Franco Agustín Rabaglia <francoagustinrabaglia@Francos-MacBook-Pro.local>
2015-09-13 17:30:10 -0300

    First commit

---
+++ ino/WebClient/WebClient.ino
@@ -0,0 +1,95 @@
+/*
+  Web client
+
+ This sketch connects to a website (http://www.google.com)
+ using an Arduino Wiznet Ethernet shield.
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe, based on work by Adrian McEwen
+
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// Enter a MAC address for your controller below.
+// Newer Ethernet shields have a MAC address printed on a sticker on the shield
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+
+// Set the static IP address to use if the DHCP fails to assign
+IPAddress ip(192, 168, 0, 177);
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+EthernetClient client;
+
+//char server[] = "hm-iot.herokuapp.com";
+char server[] = "192.168.1.100";
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  // start the Ethernet connection:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  // give the Ethernet shield a second to initialize:
+  delay(1000);
+  Serial.println("connecting...");
+}
+
+void loop()
+{
+  delay(3000);
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  if (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
+
+  post();
+
+
+}
+
+void post()
+{
+  if (client.connect(server, 3000))
+  {
+    // Make a HTTP request:
+    client.print( "GET /arduino");
+    client.println( " HTTP/1.1");
+    client.print( "Host: " );
+    client.println(server);
+    client.println( "Connection: close" );
+    client.println();
+    client.println();
+    client.stop();
+
+    if (client.connected())
+    {
+      Serial.println("Connecting to hm-iot...");
+      Serial.println();
+    }
+    else
+    {
+      Serial.println("Connection to hm-iot failed...");
+      Serial.println();
+    }
+
+  }
+}
